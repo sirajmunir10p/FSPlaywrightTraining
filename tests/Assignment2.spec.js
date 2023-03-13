739056d6c2bec04ce667df67f53d760b693efb0c
Siraj Munir <74316263+sirajmunir10p@users.noreply.github.com>
2023-03-14 02:24:31 +0500

    Update Assignment2.spec.js

--- tests/Assignment2.spec.js
+++ tests/Assignment2.spec.js
@@ -7,9 +7,11 @@ test('getting started should contain table of contents', async ({ page }) => {
   const playwrightDev = new PlaywrightDevPage(page);
   await playwrightDev.goto();
   await playwrightDev.getStarted();
+  console.log(await playwrightDev.tocList.allTextContents());
 
-  await expect(playwrightDev.tocList).toHaveText([
-    data.HowToInstall, data.WhatisInstalled, data.HowToRunExample, data.HowToOpenReport,
-    data.AssertionFixturesLocators, data.RunSingleTest, data.TestwithCodegen, data.TraceYourTests
-  ]);
+  await expect(playwrightDev.tocList).toHaveText(
+    // data.HowToInstall, data.WhatisInstalled, data.HowToRunExample, data.HowToOpenReport,
+    // data.AssertionFixturesLocators, data.RunSingleTest, data.TestwithCodegen, data.TraceYourTests //Was playing around with data
+    data.testData 
+  );
 });
